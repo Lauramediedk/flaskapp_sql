d582ece039fadb65b9a70dc5c06c02c677946182
lauramedie <4700la@gmail.com>
2023-12-05 15:46:35 +0100

    db tables structure

--- models.py
+++ models.py
@@ -10,10 +10,85 @@ def get_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-#DB table
+#DB tables
 def make_table():
     conn = get_connection()
-    conn.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)')
+    conn.execute('CREATE TABLE IF NOT EXISTS users ('
+                 'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+                 'name TEXT, '
+                 'email TEXT, '
+                 'password TEXT)'
+                 )
+    conn.commit()
+    conn.close()
+
+def friends_table():
+    conn = get_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS friends('
+                 'users_id INTEGER, '
+                 'friends_id INTEGER, '
+                 'FOREIGN KEY(users_id) REFERENCES users(id), '
+                 'FOREIGN KEY(friends_id) REFERENCES users(id)'
+                 ')')
+    conn.commit()
+    conn.close()
+
+def posts_table():
+    conn = get_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS posts('
+                 'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+                 'users_id INTEGER, '
+                 'content TEXT, '
+                 'created DATETIME DEFAULT CURRENT_TIMESTAMP, '
+                 'FOREIGN KEY(users_id) REFERENCES users(id)'
+                 ')')
+    conn.commit()
+    conn.close()
+
+def groups_table():
+    conn = get_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS groups('
+                 'id INTEGER PRIMARY KEY AUTOINCREMENT, ' 
+                 'created DATETIME DEFAULT CURRENT_TIMESTAMP, ' 
+                 'author_id INTEGER, ' 
+                 'FOREIGN KEY(author_id) REFERENCES users(id)'
+                 ')')
+    conn.commit()
+    conn.close()
+
+def users_groups():
+    conn = get_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS users_groups('
+                 'users_id INTEGER, '
+                 'group_id INTEGER, '
+                 'FOREIGN KEY(users_id) REFERENCES users(id), '
+                 'FOREIGN KEY(group_id) REFERENCES groups(id)'
+                 ')')
+    conn.commit()
+    conn.close()
+
+def rewards_table():
+    conn = get_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS rewards('
+                 'id INTEGER PRIMARY KEY AUTOINCREMENT, ' 
+                 'type TEXT, '
+                 'description TEXT' 
+                 ')')
+    conn.commit()
+    conn.close()
+
+def challenges_table():
+    conn = get_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS challenges('
+                 'id INTEGER PRIMARY KEY AUTOINCREMENT, ' 
+                 'name TEXT, '
+                 'created DATETIME DEFAULT CURRENT_TIMESTAMP, ' 
+                 'expires DATETIME DEFAULT (datetime("now", "+30 days")), ' 
+                 'topic TEXT, '
+                 'participants INTEGER, '
+                 'reward_id INTEGER, '
+                 'FOREIGN KEY(reward_id) REFERENCES rewards(id)'
+                 ')')
     conn.commit()
     conn.close()
 
