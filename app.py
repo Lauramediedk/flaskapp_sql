ea3c785acc030052fced89e65b70a0c376c90ea0
lauramedie <4700la@gmail.com>
2023-12-05 14:43:24 +0100

    structured db file and config

--- app.py
+++ app.py
@@ -2,52 +2,11 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 import sqlite3 
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import SignupForm, LoginForm
+from models import get_connection, make_table, validate_user, check_for_emails, register_user_db
+from config import DATABASE
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
-DATABASE = 'database.db'
-
-#DB connect
-def get_connection():
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    conn.row_factory = sqlite3.Row
-    return conn
-
-#DB table
-def make_table():
-    conn = get_connection()
-    conn.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)')
-    conn.commit()
-    conn.close()
-
-#Validering
-def validate_user(email, password):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT password FROM users WHERE email = ?', (email,))
-    result = cursor.fetchone()  # Fetch the hashed password
-    conn.close()
-
-    if result:
-        hashed_password = result[0]
-        if check_password_hash(hashed_password, password):
-            return True #Passwords matcher
-        return False #Intet match med password eller user
-    
-#Email validering for signup
-def check_for_emails(email):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
-    result = cursor.fetchone()
-    conn.close
-
-    if result:
-        print("Existing email found:", email)  # Add this line for debugging purposes
-        return True
-
-    return False
 
 #Routes
 @app.route("/")
@@ -115,20 +74,6 @@ def signup():
             return redirect(url_for('signup'))
 
     return render_template('signup.html', form=form)
-
-#Registrer bruger i DB
-def register_user_db(name, email, hashed_password):
-    conn = get_connection()
-    cursor = conn.cursor()
-    try:
-        cursor.execute('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', (name, email, hashed_password))
-        conn.commit()
-        return True 
-    except sqlite3.Error as e:
-        print(f"Error inserting user: {e}")
-        return False
-    finally: 
-        conn.close()
 
 @app.route("/challenges")
 def challenges():
