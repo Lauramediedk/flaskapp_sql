6668e273a86faf40daf63c83c767579d0fb33bcb
lauramedie <4700la@gmail.com>
2023-12-14 13:15:49 +0100

    new changes

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, Email
 
@@ -25,3 +26,9 @@ class LoginForm(FlaskForm):
         #render_kw --> tilføj ekstra felt i ens html som placeholder eller en class
 
     submit = SubmitField("Log ind")
+
+#Make post form
+class PostForm(FlaskForm):
+    content = StringField(validators=[InputRequired()])
+    image_path = FileField(validators=[FileAllowed(['jpg', 'png'], 'jpg og png format kun tilladt')])
+    submit = SubmitField("Slå op")
