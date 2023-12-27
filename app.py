b9d1f223b5af4020cd497051592f2bb1dd7a9524
lauramedie <4700la@gmail.com>
2023-12-27 11:07:13 +0100

    app.py fixes for flake8 warnings

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash, abort, send_from_directory
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import (
+    Flask, redirect, url_for, render_template,
+    request, session, flash, abort, send_from_directory
+)
+from werkzeug.security import generate_password_hash
 from werkzeug.utils import secure_filename
 from forms import SignupForm, LoginForm, PostForm, FitnessForm
 from datetime import datetime
 import models
 import os
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'mysecretkey'
     app.config['UPLOAD_FOLDER'] = 'uploads'
 
-    #Vi laver vores eget custom filter for dato og bruger Python's datetime
+    # Vi laver vores eget custom filter for dato og bruger Python's datetime
     def datetime_format(value):
         date_obj = datetime.strptime(value, '%Y-%m-%d')
         return date_obj.strftime("%d %B")
 
     app.jinja_env.filters['datetimeformat'] = datetime_format
 
-    #Routes
+    # Routes
     @app.route("/")
     def index():
         return render_template("index.html")
@@ -28,7 +32,7 @@ def create_app():
         if not is_logged_in():
             flash('Du skal være logget ind for at tilgå dashboard', 'error')
             return redirect(url_for('login'))
-        
+
         user_id = session['user_id']
         user_id = session['user_id']
         rewards = models.get_rewards()
@@ -38,8 +42,8 @@ def create_app():
         follows = models.get_users_follow(user_id)
         users_fitness = models.get_users_fitness(user_id)
         form = FitnessForm()
-
-        #Hent navnet på title feltet i vores rewards, hvis der er nogle associeret i users_rewards, ellers er den empty
+        # Hent navnet på title feltet i vores rewards,
+        # hvis der er nogle associeret i users_rewards, ellers er den empty
         users_rewards = [reward[0] for reward in users_rewards] if users_rewards else []
 
         if request.method == 'POST':
@@ -48,23 +52,30 @@ def create_app():
                 calories_burned = form.calories.data
                 models.add_fitness_data(user_id, distance, calories_burned)
                 flash('Data uploadet', 'success')
-                return redirect(url_for('dashboard')) 
-            #Vi laver redirect med det nye data, og undgår resubmission når refresh af siden sker.
+                return redirect(url_for('dashboard'))
+            # Vi laver redirect med det nye data, og undgår resubmission når refresh af siden sker.
             else:
                 flash('Data kunne ikke uploades', 'error')
-        #Vi render det hele med template, og tjekker med if i vores template
-        return render_template("dashboard.html", form=form, rewards=rewards, users_rewards=users_rewards, challenges=challenges, user_posts=user_posts, follows=follows, users_fitness=users_fitness)
+        # Vi render det hele med template, og tjekker med if i vores template
+        return render_template(
+            "dashboard.html",
+            form=form,
+            rewards=rewards,
+            users_rewards=users_rewards,
+            challenges=challenges,
+            user_posts=user_posts,
+            follows=follows,
+            users_fitness=users_fitness
+        )
 
-
-    @app.route("/dashboard/<int:post_id>", methods=['DELETE'])  
+    @app.route("/dashboard/<int:post_id>", methods=['DELETE'])
     def delete_dashboard_post(post_id):
         if models.delete_post_db(post_id, session['user_id']):
             return ''
         else:
             abort(403)
 
-
-    @app.route("/dashboard/unfollow/<int:friends_id>", methods=['DELETE'])  
+    @app.route("/dashboard/unfollow/<int:friends_id>", methods=['DELETE'])
     def unfollow_users(friends_id):
 
         users_id = session['user_id']
@@ -75,7 +86,6 @@ def create_app():
             flash('Handling fejlede', 'error')
             abort(403)
 
-
     #Login
     @app.route("/login", methods=['GET', 'POST'])
     def login():
@@ -85,13 +95,13 @@ def create_app():
             password = form.password.data
             if models.validate_user(email, password):
                 session['email'] = email
-                #Hent id og navn på bruger
+                # Hent id og navn på bruger
                 conn = models.get_connection()
                 cursor = conn.cursor()
                 cursor.execute('SELECT id, name FROM users WHERE email = ?', (email,))
                 user = cursor.fetchone()
                 conn.close()
-                #Brug info i session
+                # Brug info i session
                 if user:
                     user_id = user['id']
                     user_name = user['name']
@@ -107,7 +117,7 @@ def create_app():
             
         return render_template('login.html', form=form)
 
-    def is_logged_in(): #Check om email er inkluderet i vores session
+    def is_logged_in(): # Check om email er inkluderet i vores session
         return 'email' in session
 
     #Signup
@@ -119,11 +129,11 @@ def create_app():
             email = form.email.data
             password = form.password.data
             hashed_password = generate_password_hash(password)
-            #Check for eksisterende email
+            # Check for eksisterende email
             if models.check_for_emails(email):
                 flash('Den indtastede email er allerede i brug', 'error')
                 return render_template('signup.html', form=form)
-            #Hvis alt er godkendt
+            # Hvis alt er godkendt
             if models.register_user_db(name, email, hashed_password):
                 flash('Registrering godkendt', 'succes')
                 return redirect(url_for('login'))
@@ -147,7 +157,7 @@ def create_app():
             no_challenges_found = "Der er i øjeblikket ingen udfordringer"
             return render_template("challenges.html", no_challenges_found=no_challenges_found)
 
-    #join challenge
+    # join challenge
     @app.route("/join_challenge/<int:challenges_id>", methods=['POST'])
     def join_challenge(challenges_id):
         user_id = session.get('user_id')
@@ -159,7 +169,6 @@ def create_app():
 
         return redirect(url_for('challenges'))
 
-
     @app.route("/feed", methods=['GET', 'POST'])
     def feed():
         if not is_logged_in():
@@ -167,13 +176,12 @@ def create_app():
             return redirect(url_for('login'))
         return render_template("feed.html")
 
-
     @app.route("/people", methods=['GET', 'POST'])
     def people():
         if not is_logged_in():
             flash('Du skal være logget ind for at tilgå feed', 'error')
             return redirect(url_for('login'))
-        
+
         if request.method == 'POST':
             search = request.form['search']
             users = models.get_users(search)
@@ -182,8 +190,7 @@ def create_app():
 
         return render_template("people.html", users=users)
 
-
-    #Tilføj venner og fjern venner
+    # Tilføj venner og fjern venner
     @app.route("/people/<int:friends_id>", methods=['POST'])
     def follow_users(friends_id):
 
@@ -196,7 +203,6 @@ def create_app():
             flash('Person tilføjet', 'success')
 
         return redirect(url_for('people'))
-
 
     @app.route("/posts", methods=['GET', 'POST'])
     def posts():
@@ -242,26 +248,23 @@ def create_app():
         else:
             flash('Ingen opslag i øjeblikket', 'error')
             return render_template("posts.html", form=form)
-        
-    #Image
+
+    # Image
     @app.route("/uploads/<filename>")
     def uploaded_file(filename):
         return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-
-    @app.route("/posts/<int:post_id>", methods=['DELETE'])  
+    @app.route("/posts/<int:post_id>", methods=['DELETE'])
     def delete_post(post_id):
         if models.delete_post_db(post_id, session['user_id']):
             return ''
         else:
             abort(403)
 
-
     @app.route("/logout")
     def logout():
         session.clear()
         return redirect(url_for('index'))
-
 
 
     return app
